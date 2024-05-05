65f09b599982031b56e8f75b8faedff42258f478
Ibrahim Gidi <114783401+BlackLionXD@users.noreply.github.com>
2024-05-05 21:03:34 +0300

    assignment works successfully

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 
 
 
-"use strict";
-
 var app = angular.module('rootModule',[]);
 
-app.controller('helloController', function($scope){
-    $scope.message = 'Hello World'
-})
+app.controller('helloController', ($scope)=>{
+$scope.message = 'hello world';
 
+});
+
+app.controller('formController',($scope)=>{
+    $scope.checkLunch = function() {
+        var lunchItems = $scope.lunchMenu ? $scope.lunchMenu.split(',') : [];
+        if (lunchItems.length === 0 || (lunchItems.length === 1 && lunchItems[0].trim() === '')) {
+            $scope.message = 'Please enter data first';
+        } else if (lunchItems.length <= 3) {
+            $scope.message = 'Enjoy!';
+        } else {
+            $scope.message = 'Too much!';
+        }
+    };
+})
